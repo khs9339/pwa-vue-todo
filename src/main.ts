b247e9980fa9a92766af424c115e4b3f6e04738a
김효성 <hyo@wemakeprice.com>
2018-09-17 17:50:27 +0900

    test-1-main

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,7 @@ import App from './App.vue';
 import router from './router';
 import store from './store';
 import './registerServiceWorker';
-
+// test -1 
 Vue.config.productionTip = false;
 
 new Vue({
